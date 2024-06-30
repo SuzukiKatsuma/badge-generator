8dec9c2845852e7b725c951de4ae52c3e2e63b2d
SuzukiKatsuma <71871253+SuzukiKatsuma@users.noreply.github.com>
2024-06-30 21:20:20 +0900

    fix: vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
-  base: '/',
   plugins: [react()],
+  root: 'src',
   publicDir: 'public',
 });
