c6c4b80be6788036d8a81c45e0b4790ec2045f87
SuzukiKatsuma <71871253+SuzukiKatsuma@users.noreply.github.com>
2024-07-01 00:02:17 +0900

    chore: path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: 'src',
   publicDir: 'public',
+  resolve: {
+    alias: {
+      '~/': path.join(__dirname, 'src/'),
+    },
+  },
 });
